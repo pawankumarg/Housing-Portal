f9ebf27343d207fdb8e7ba7f95a2e214273e7571
pawankumarg <simplypawangupta@gmail.com>
2017-01-13 10:07:20 +0530

    added auth.js

--- config/auth.js
+++ config/auth.js
@@ -1,12 +1,12 @@
 module.exports = {
 	facebookAuth : {
-    	'clientID': "964399293595510",
-    	'clientSecret': "8d591a7c43fdace42dfed8590476c4bb",
+    	'clientID': "",
+    	'clientSecret': "",
     	'callbackURL': "http://www.easehousing.com/auth/facebook/callback",
 	},
 	googleAuth : {
-	 	'clientID': "415805634360-t6khv8j2d08rak45c7mlafdq3reitker.apps.googleusercontent.com",
-    	'clientSecret': "XIlzNUBbwL8rBq-F2btbJC0J",
+	 	'clientID': "",
+    	'clientSecret': "",
     	'callbackURL': "http://www.easehousing.com/auth/google/callback"
 	}
 
